a4356efad2db2bbe324c14616d64f865a298bef4
cesarledres <cesarledrescv@gmail.com>
2026-04-11 23:42:57 -0300

    Organização de pastas e arquivos do repositório.

--- exercicios de if e else/exemplos_estrutura_decisao_1/programas/exemplo3.py
+++
@@ -1,8 +0,0 @@
-# Exemplo 3: Verificar e exibir se um numero eh diferente ou igual a zero
-numero = int(input("Informe um numero inteiro: "))
-
-if (numero != 0):
-    print("O numero eh diferente de zero")
-else:
-    print("O numero eh igual a zero")
-
